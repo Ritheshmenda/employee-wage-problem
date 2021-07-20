e50c8d96f28ec5e2c2e7c05d0b0de2cefae299ad
Ritheshmenda <ritheshmenda123@gmail.com>
2021-07-20 19:48:35 +0530

    [Rithesh]added:welcome

---
+++ Employeewage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace employee_wage_problem
+{
+    class Employeewage
+    {
+        public void Introduction()
+        {
+            Console.WriteLine("welcome to employee computation problem");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace employee_wage_problem
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Employeewage Employ = new Employeewage();
+            Employ.Introduction();
+        }
+
+    }
+}
